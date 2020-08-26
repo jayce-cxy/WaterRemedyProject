e720daa44089197bda0959a523bb7099c2d4d44d
jayce-cxy <54123291+jayce-cxy@users.noreply.github.com>
2020-08-26 12:30:01 +1000

    Initialize the project
    upload the initialize the project and gitignore

---
+++ WaterRemedy/WaterRemedy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WaterRemedy.Startup))]
+namespace WaterRemedy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
