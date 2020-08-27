0d44c8e17fd1d08df3910d5b4bafa73f384d437f
jayce-cxy <54123291+jayce-cxy@users.noreply.github.com>
2020-08-27 21:14:45 +1000

    update landing page
    design the landing page.

--- WaterRemedy/WaterRemedy/Properties/AssemblyInfo.cs
+++ WaterRemedy/WaterRemedy/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25301d5e-3c5b-4893-893c-05acab4dd8e3")]
+[assembly: Guid("b23890ee-cd68-499b-ae65-8b3c4e238768")]
 
 // Version information for an assembly consists of the following four values:
 //
