ab907f45c6773becaa1fbe83f1eccb8ff171c26d
jayce-cxy <54123291+jayce-cxy@users.noreply.github.com>
2020-08-30 04:48:47 +1000

    Data visualization
    update the Data visualization

--- WaterRemedy/WaterRemedy/Controllers/HomeController.cs
+++ WaterRemedy/WaterRemedy/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WaterRemedy.Utility;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace WaterRemedy.Controllers
 {
@@ -24,6 +28,111 @@ namespace WaterRemedy.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult homePage() {
+
+            return View();
+        }
+
+        // testing
+        [HttpGet]
+        public ActionResult GetAustralianWaterUsage()
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(StaticFilePath.LocalFilePath);
+                var fileInfo = directoryInfo.GetFiles().FirstOrDefault(x => x.Name.Equals("Aus_Water_Usage.csv"));
+                if (fileInfo == null) return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = "No Data" }), "application/json");
+
+                var process = new ImportCSV($@"{fileInfo.DirectoryName}\", fileInfo.Name);
+                var data = process.ReadCsvFileToTable();
+
+                return Content(JsonConvert.SerializeObject(new { isSuccess = true, Data = data }), "application/json");                
+            }
+            catch (Exception e)
+            {
+                return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = e.Message }), "application/json");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetWaterChemical()
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(StaticFilePath.LocalFilePath);
+                var fileInfo = directoryInfo.GetFiles().FirstOrDefault(x => x.Name.Equals("Melbourne_water_chemicals.csv"));
+                if (fileInfo == null) return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = "No Data" }), "application/json");
+
+                var process = new ImportCSV($@"{fileInfo.DirectoryName}\", fileInfo.Name);
+                var data = process.ReadCsvFileToTable();
+
+                return Content(JsonConvert.SerializeObject(new { isSuccess = true, Data = data }), "application/json");
+            }
+            catch (Exception e)
+            {
+                return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = e.Message }), "application/json");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetVicWaterUsage()
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(StaticFilePath.LocalFilePath);
+                var fileInfo = directoryInfo.GetFiles().FirstOrDefault(x => x.Name.Equals("Victoria_Water_usage.csv"));
+                if (fileInfo == null) return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = "No Data" }), "application/json");
+
+                var process = new ImportCSV($@"{fileInfo.DirectoryName}\", fileInfo.Name);
+                var data = process.ReadCsvFileToTable();
+
+                return Content(JsonConvert.SerializeObject(new { isSuccess = true, Data = data }), "application/json");
+            }
+            catch (Exception e)
+            {
+                return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = e.Message }), "application/json");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetVicWaterUsagePc()
+        {
+            try
+            {
+                var directoryInfo = new DirectoryInfo(StaticFilePath.LocalFilePath);
+                var fileInfo = directoryInfo.GetFiles().FirstOrDefault(x => x.Name.Equals("Aus_Water_Usage_pc.csv"));
+                if (fileInfo == null) return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = "No Data" }), "application/json");
+
+                var process = new ImportCSV($@"{fileInfo.DirectoryName}\", fileInfo.Name);
+                var data = process.ReadCsvFileToTable();
+
+                return Content(JsonConvert.SerializeObject(new { isSuccess = true, Data = data }), "application/json");
+            }
+            catch (Exception e)
+            {
+                return Content(JsonConvert.SerializeObject(new { isSuccess = false, Message = e.Message }), "application/json");
+            }
+        }
+
+        public ActionResult chemicalView()
+        {
+            return View();
+        }
+
+        public ActionResult AusWaterUsage()
+        {
+            return View();
+        }
+
+        public ActionResult PercentageOfUsage()
+        {
+            return View();
+        }
+        public ActionResult MelWaterUsage()
+        {
             return View();
         }
     }
---
+++ WaterRemedy/WaterRemedy/Utility/Utility.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WaterRemedy.Utility
+{
+    public static class StaticFilePath
+    {
+        public static string LocalFilePath = $@"{AppDomain.CurrentDomain.BaseDirectory}\App_Data";
+    }
+
+    public class ImportCSV
+    {
+        private string path;
+
+        private string fileName;
+
+        public ImportCSV(string filePath, string fileName)
+        {
+            this.path = filePath;
+            this.fileName = fileName;
+        }
+
+        public DataTable ReadCsvFileToTable()
+        {
+            return ReadCsvFileToTable(true, ',');
+        }
+
+        public DataTable ReadCsvFileToTable(bool HeadYes, char span)
+        {
+            
+            string files = path + fileName;
+            DataTable dt = new DataTable();
+            StreamReader fileReader = new StreamReader(files, Encoding.Default);
+            try
+            {
+                
+                int lsi = 0;
+                
+                char cv = span;
+                while (fileReader.EndOfStream == false)
+                {
+                    string line = fileReader.ReadLine();
+                    string[] y = line.Split(cv);
+                    
+                    if (HeadYes == true)
+                    {
+                        
+                        if (lsi == 0)
+                        {
+                            for (int i = 0; i < y.Length; i++)
+                            {
+                                dt.Columns.Add(y[i].Trim().ToString());
+                            }
+                            lsi++;
+                        }
+                        
+                        else
+                        {
+                            DataRow dr = dt.NewRow();
+                            for (int i = 0; i < y.Length; i++)
+                            {
+                                dr[i] = y[i].Trim();
+                            }
+                            dt.Rows.Add(dr);
+                        }
+                    }
+
+                    
+
+                    else
+                    {
+
+                        if (lsi == 0)
+                        {
+                            for (int i = 0; i < y.Length; i++)
+                            {
+                                dt.Columns.Add(i.ToString());
+                            }
+                            lsi++;
+                        }
+
+                        DataRow dr = dt.NewRow();
+
+                        for (int i = 0; i < y.Length; i++)
+                        {
+                            dr[i] = y[i].Trim();
+                        }
+                        dt.Rows.Add(dr);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            finally
+            {
+                fileReader.Close();
+                fileReader.Dispose();
+            }
+            return dt;
+
+        }
+
+    }
+}
